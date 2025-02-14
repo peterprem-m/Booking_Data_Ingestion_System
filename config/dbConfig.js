021a7c6082ad9d19602c42fe26c0e6b0bd33d3e7
peterprem-m <premkumar.m@techouts.co.in>
2025-02-14 20:43:11 +0530

    Removed consoles

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = async() => {
-    try{
-        if(!process.env.MONGO_URI){
-            console.log(process.env.MONGO_URI);
-            console.log('Not configured')
-        }
-        else{
-            console.log('got it')
+    try{   
         await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
         console.log('DB Connected');
-        }
     }
     catch(error) {
         console.error('Connection error', error);
